0dc8495b8af14794a549d2f46a49f7bd6ff20b61
Lorenzo Gardini <lorenzo.gardini7@studio.unibo.it>
2024-06-30 20:35:16 +0200

    fix: html to pdf

--- convert_md_to_pdf.sh
+++ convert_md_to_pdf.sh
@@ -10,6 +10,7 @@ for file in $(find report -type f -name '*.md'); do
   output_file="${base_filename%.md}.pdf"
   output_dir=$(echo "$base_output_dir" | sed 's/report/output/')
   mkdir -p "$output_dir"
-  pandoc -f gfm -t html5 --metadata pagetitle="file.md" -s --css="https://gist.githubusercontent.com/killercup/5917178/raw/40840de5352083adb2693dc742e9f75dbb18650f/pandoc.css" "$file" -o "$output_dir/$output_file"
+  pandoc -f gfm -t html5 --metadata pagetitle="file.md" -s --css="https://gist.githubusercontent.com/killercup/5917178/raw/40840de5352083adb2693dc742e9f75dbb18650f/pandoc.css" --from markdown+raw_attribute+link_attributes+citations
+   "$file" -o "$output_dir/$output_file"
   #pandoc -f gfm -t html5 --metadata pagetitle="file.md" -css="report/white.css" "$file" -o "$output_dir/$output_file" 
 done
