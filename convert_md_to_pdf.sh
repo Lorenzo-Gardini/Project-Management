1d1dc3462542bda6f6be293373126851f75f1658
Lorenzo Gardini <lorenzo.gardini7@studio.unibo.it>
2024-06-30 19:51:26 +0200

    update: revert pandoc but with css

--- convert_md_to_pdf.sh
+++ convert_md_to_pdf.sh
@@ -6,5 +6,5 @@ for file in $(find report -type f -name '*.md'); do
   output_file="${base_filename%.md}.pdf"
   output_dir=$(echo "$base_output_dir" | sed 's/report/output/')
   mkdir -p "$output_dir"
-  docker run --rm --volume "`pwd`:/data" pandoc/latex "$file" -o "$output_dir/$output_file"
+  docker run --rm --volume "`pwd`:/data" pandoc/latex "$file" -o "$output_dir/$output_file" --standalone --css=report/white.css
 done
