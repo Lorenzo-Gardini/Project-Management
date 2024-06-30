991e6481a5eb1d588fabcb4cab3ccccc456ff1ff
Lorenzo Gardini <lorenzo.gardini7@studio.unibo.it>
2024-06-30 18:48:43 +0200

    fix: change opensans

--- convert_md_to_pdf.sh
+++ convert_md_to_pdf.sh
@@ -6,5 +6,5 @@ for file in $(find report -type f -name '*.md'); do
   output_file="${base_filename%.md}.pdf"
   output_dir=$(echo "$base_output_dir" | sed 's/report/output/')
   mkdir -p "$output_dir"
-  docker run --rm --volume "`pwd`:/data" pandoc/latex "$file" -o "$output_dir/$output_file" --metadata-file=report/metadata.yml --csl=report/ieee.csl --pdf-engine=xelatex --css=report/opensans.css
+  docker run --rm --volume "`pwd`:/data" pandoc/latex "$file" -o "$output_dir/$output_file" --standalone --metadata-file=report/metadata.yml --csl=report/ieee.csl --pdf-engine=xelatex --css=report/opensans.css
 done
