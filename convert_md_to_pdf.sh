f23a5a442ed63abd75a40945b0ef5d8f63916f5c
Lorenzo Gardini <lorenzo.gardini7@studio.unibo.it>
2024-06-30 20:41:54 +0200

    fix: html to pdf

--- convert_md_to_pdf.sh
+++ convert_md_to_pdf.sh
@@ -2,7 +2,6 @@
 sudo apt update
 sudo apt install wkhtmltopdf
 sudo apt install pandoc
-wget https://raw.githubusercontent.com/simov/markdown-viewer/master/themes/github.css
 
 for file in $(find report -type f -name '*.md'); do
   base_filename=$(basename "$file")
@@ -10,6 +9,5 @@ for file in $(find report -type f -name '*.md'); do
   output_file="${base_filename%.md}.pdf"
   output_dir=$(echo "$base_output_dir" | sed 's/report/output/')
   mkdir -p "$output_dir"
-  pandoc -f gfm -t html5 --metadata pagetitle="file.md" -s --css="https://gist.githubusercontent.com/killercup/5917178/raw/40840de5352083adb2693dc742e9f75dbb18650f/pandoc.css" --from markdown+raw_attribute+link_attributes+citations "$file" -o "$output_dir/$output_file"
-  #pandoc -f gfm -t html5 --metadata pagetitle="file.md" -css="report/white.css" "$file" -o "$output_dir/$output_file" 
+  pandoc -f gfm -t html5 --metadata pagetitle="file.md" -s --css="https://sitolorenzogardini.altervista.org/white.css" --from markdown+raw_attribute+link_attributes+citations "$file" -o "$output_dir/$output_file"
 done
