23ce8d5b91f460218e617d6993d7e903b3c26c6f
Lorenzo Gardini <lo.gardini@reply.it>
2024-07-30 15:38:15 +0200

    try to update script

--- convert_md_to_html.sh
+++ convert_md_to_html.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo apt update
-#sudo apt install wkhtmltopdf
 sudo apt install pandoc
 
 # Iterate over all files found in the 'report' directory
@@ -11,20 +10,18 @@ for file in $(find report -type f); do
   
   # Determine the output directory by replacing 'report' with 'output'
   output_dir=$(echo "$base_output_dir" | sed 's/report/output/')
+  mkdir -p "$output_dir"
 
   # Check if the file is a Markdown file
   if [[ "$file" == *.md ]]; then
+    echo "Converting $file"
     # Define the output file name with .html extension
-    output_file="${base_filename%.md}.html"
-    
-    # Create the output directory if it doesn't exist
-    mkdir -p "$output_dir"
-    
+    output_file="${base_filename%.md}.html"    
     # Convert the Markdown file to HTML using Pandoc
-    pandoc -f markdown -t html5 --metadata pagetitle="$base_filename" -s --css="https://cdn.jsdelivr.net/gh/Lorenzo-Gardini/Project-Management/report/pandoc.css" --from markdown+raw_attribute+link_attributes "$file" -o "$output_dir/$output_file"
+  pandoc -f markdown -t html5 --metadata pagetitle="file" -s --css="https://cdn.jsdelivr.net/gh/Lorenzo-Gardini/Project-Management/report/pandoc.css" --from markdown+raw_attribute+link_attributes "$file" -o "$output_dir/$output_file"
   else
     # For non-Markdown files, copy them to the corresponding output directory
-    mkdir -p "$output_dir" # Ensure the directory exists
+    echo "Coping $file"
     cp "$file" "$output_dir/"
   fi
 done
